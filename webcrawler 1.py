a01f2fd3f85e9fb26c5451936afb5740eb5edd72
Chotalia <rchotalia@dxc.com>
2020-06-09 11:16:14 +0100

    Crawl function took 59secs; still slow

--- webcrawler 1.py
+++ webcrawler 1.py
@@ -1,5 +1,7 @@
 import httpx
 import asyncio
+import time
+from typing import Callable, Coroutine
 
 
 async def crawl0(
@@ -16,13 +18,59 @@ async def crawl0(
         if line.startswith(prefix):
             await crawl0(prefix, line)
 
-asyncio.run(crawl0(
-    'https://langa.pl/crawl/'
-))
-#
-# Not so good doing it this way because:
-# Takes a long while - downloads happening slowly.
-#   1) Back-end activities also responsible for reporting their status; reporting progress from the same task - not good
-#   await in crawl is within the crawl0 so this is recursive waits - not good
-#   Also, not using concurrencies since we are aawiting crawl0
-#   Should use context manager to call awaits rather than direct code.
+addr = 'https://langa.pl/crawl/'
+
+# browsing to addr returns another address
+# https://langa.pl/crawl/0
+# then this has two further addresses
+# https://langa.pl/crawl/00
+# https://langa.pl/crawl/01
+# etc.
+
+# Frameworks expecting callback functions of specific signatures might be type hinted using Callable[[Arg1Type, Arg2Type], ReturnType].
+# https://docs.python.org/3/library/typing.html
+
+
+async def progress(
+    url: str,
+    algo: Callable[..., Coroutine],
+):
+    asyncio.create_task(
+        algo(url),
+        name=url,
+    )
+    todo.add(url)
+    start = time.time()
+    while len(todo):
+        print(
+            f'{len(todo)}: '
+            + ', '.join(
+                sorted(todo)
+            )[-38:]
+        )
+        await asyncio.sleep(0.5)
+    end = time.time()
+    print(f'Took {int(end - start)}'
+          + ' seconds')
+
+todo = set()
+
+
+async def crawl1(
+    prefix: str, url: str = ''
+):
+    url = url or prefix
+    client = httpx.AsyncClient()
+    try:
+        res = await client.get(url)
+    finally:
+        await client.aclose()
+    for line in res.text.splitlines():
+        if line.startswith(prefix):
+            todo.add(line)
+            await crawl1(prefix, line)
+        todo.discard(url)
+
+asyncio.run(progress(addr, crawl1))
+
+# This is still slow though : Took 59 seconds!

73a6baf741b51abce73409c9c027d4caa72a4b18
Chotalia <rchotalia@dxc.com>
2020-06-09 08:27:32 +0100

    gather with try/except timeout

--- script 4.py
+++ script 4.py
@@ -16,13 +16,41 @@ async def keep_printing(name):
         await asyncio.sleep(0.50)
 
 
-async def async_main():
-    await asyncio.gather(
-        keep_printing('First'),
-        keep_printing('Second'),
-        keep_printing('Third')
-    )
+# async def async_main():
+#     await asyncio.gather(
+#         keep_printing('First'),
+#         keep_printing('Second'),
+#         keep_printing('Third')
+#     )
 
-# Single thread executes multiple coroutines at the same time
+# # Single thread executes multiple coroutines at the same time
+# asyncio.run(async_main())
+# # Carries on indefinitely - interrupt via keyboard to stop
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Now use a timeout within a try/except construct
+
+async def async_main():
+    try:
+        await asyncio.wait_for(
+            asyncio.gather(
+                keep_printing('First'),
+                keep_printing('Second'),
+                keep_printing('Third')
+            ),
+            5
+        )
+    except asyncio.TimeoutError:
+        print('Timed Out!')
+
 asyncio.run(async_main())
-# Carries on indefinitely - interrupt via keyboard to stop
+
+# When this runs, get :
+# Timed Out!
+# _GatheringFuture exception was never retrieved
+# future: <_GatheringFuture finished exception=CancelledError()>
+# asyncio.exceptions.CancelledError
+# this is because the Timeout cancels the gather and this cancellation
+# propagates to the keep_printing coroutines within it.
+# We must handel these as well. Cancellations are errored.
